55d8df3b4aed4a162c449346b10db0f623906be8
BillAndyQ <billandyqm@gmail.com>
2024-08-20 20:02:21 -0500

    appadmin

--- adminApp/urls.py
+++ adminApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Cambia 'home' por la vista que desees
+    path('', views.home, name='admin'),  # Cambia 'home' por la vista que desees
     # Otras rutas de la app
 ]
